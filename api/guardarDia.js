199cf18f2462d9be2b00369c1287ee25aeac497b
Syla-av <alyssonavil@gmail.com>
2025-11-27 22:21:53 -0500

    Update guardarDia.js

--- api/guardarDia.js
+++ api/guardarDia.js
@@ -1,4 +1,3 @@
-// api/guardarDia.js
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
@@ -29,9 +28,9 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
-
     res.status(200).json({ ok: true, data });
   } catch (err) {
     res.status(500).json({ ok: false, error: err.message });
   }
 }
+
